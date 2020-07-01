2e689ff24d59705db1890bba1cb62a7e6285cb86
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-07-01 14:08:55 +1000

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Bogdan on 1/7/20.
+//  Copyright © 2020 Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
